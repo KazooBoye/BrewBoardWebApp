047816d346da5332366abd7715c588ac4e5f4d32
Kuan-niisan <haibakhongtam2004@gmail.com>
2024-05-08 12:44:01 +0700

    small update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Register from './pages/Register';
-import ShopPage from './pages/ShopPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import ShopPage from "./pages/ShopPage";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/shop" element={<ShopPage />} />
-            </Routes>
-        </Router>
-    );
-};
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,3 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
 const Register = () => {
   return (
     <>
