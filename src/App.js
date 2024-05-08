6194c0559ff4b9d45ee37e30fd2467265f1ab757
Cao Duc Anh <ducanh127nps@gmail.com>
2024-05-08 13:43:14 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage'; // Import your home page component
-import Register from './Register'; // Import your register page component
+import HomePage from './pages/HomePage';
+import Register from './pages/Register';
+import ShopPage from './pages/ShopPage';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage />} /> {/* Render HomePage component when URL matches / */}
-                <Route path="/register" element={<Register />} /> {/* Render RegisterPage component when URL matches /register */}
+                <Route path="/" element={<HomePage />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/shop" element={<ShopPage />} />
             </Routes>
         </Router>
     );
-}
+};
 
 export default App;
--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,19 @@
 import { Card, ListGroup, ListGroupItem, Container, Row, Col } from 'react-bootstrap';
 import './BrewBoardLogo.png'
+import { useNavigate } from 'react-router-dom'
 
 function Body() {
+
+const navigate = useNavigate();
+
+const HandleRegister = () => {
+    navigate("/shop")
+    }
     return(
         <Container>
             <Row className='align-items-center'>
                 <Col>
-                <Card className='rounded' style={{width: '16rem'}}>
+                <Card onClick={HandleRegister} className='rounded' style={{width: '16rem'}}>
                     <Card.Img variant='top' src="BrewBoardLogo.png" width='300px' height='250px' alt="" />
                     <Card.Body>
                             <Card.Title>Coffee Shop Name</Card.Title>
